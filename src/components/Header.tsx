2e846c7d4b5e88af33e4e8bda0e97bff33e4770a
Nathan Baek <baeks24@gsiscommunity.kr>
2025-06-19 22:52:49 -0400

    Editing header to add logo and nav bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,17 +2,32 @@ import React from 'react';
 
 
 const Header: React.FC = () => {
+
+const logo = require('./img/logo.png');
+
   return (
     <header className="header">
-        <h1 className="header-title">TPS</h1>
-        <nav className="header-nav">
-            <ul>
-            <li><a href="/landing">Landing</a></li>
-            <li><a href="/project">Project</a></li>
-            <li><a href="/resource">Resource</a></li>
-            <li><a href="/team">Team</a></li>
-            </ul>
-        </nav>
+        <div className="header-container">
+          <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'space-between', width: '95%' }}> {/*Fix width later with padding CSS*/}
+
+            <div className="logo-container">
+              <a href="/landing" className="header-logo-link">
+              <img src={logo} alt="logo" className="header-logo" height={200}></img>
+              </a>
+            </div>
+            <div className="nav-container" style={{ display: 'flex', alignItems: 'center' }}>
+              <nav className="header-nav">
+              <ul>
+                <li><a href="/landing">Landing</a></li>
+                <li><a href="/project">Project</a></li>
+                <li><a href="/resource">Resource</a></li>
+                <li><a href="/team">Team</a></li>
+                </ul>
+              </nav>
+            </div>
+          </div>
+        </div>
+        <br></br>
     </header>
   );
 };
