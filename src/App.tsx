9fc1de9a1e9c64e85ecfc1c33443fbec68d5a4e0
Nathan Baek <baeks24@gsiscommunity.kr>
2025-06-19 21:53:00 -0400

    Installing react router dom package

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 
 function App() {
