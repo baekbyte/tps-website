e8e7822dce12c08367a5c8b89c38bc1b44e7a814
Nathan Baek <baeks24@gsiscommunity.kr>
2025-06-30 15:20:10 -0400

    Adding sample members

--- src/components/TeamPage.tsx
+++ src/components/TeamPage.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
+import './TeamPage.css';
+import TeamCard from './TeamCards';
 
 const TeamPage: React.FC = () => {
 
@@ -12,9 +14,39 @@ const TeamPage: React.FC = () => {
                 <div className="header-content">
                     <div className="header-text">
                         <h1 className="header-title">
-                            <span className="title-line">Team</span>
+                            <span className="title-line">Meet the Team</span>
                         </h1>
                     </div>
+                </div>
+            </section>
+            {/* Section 2: Team Cards */}
+            <section className="team-cards-section">
+                <div className="team-cards-container">
+                    <TeamCard
+                        image='/headshots/nathan.JPG'
+                        name="Amelia Frank"
+                        title="President"
+                        affiliation="Johns Hopkins University"
+                        linkedin="https://linkedin.com"
+                        email="jane.doe@email.com"
+                    />
+                    <TeamCard
+                        image='/headshots/nathan.JPG'
+                        name="Seokhyun (Nathan) Baek"
+                        title="Vice President"
+                        affiliation="Johns Hopkins University"
+                        linkedin="https://www.linkedin.com/in/baekseokhyun/"
+                        email="sbaek19@jh.edu"
+                    />
+                    <TeamCard
+                        image='/headshots/nathan.JPG'
+                        name="Idris Sunmola"
+                        title="Head of Research"
+                        affiliation="Johns Hopkins University"
+                        linkedin="https://linkedin.com"
+                        email="jane.doe@email.com"
+                    />
+                    {/* Add more <TeamCard ... /> for each member */}
                 </div>
             </section>
         </div>
